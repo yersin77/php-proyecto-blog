0505b939e7f77b5203dfb8cd1963dcb2bbdf584c
jonn-pc <joliversalamanca@gmail.com>
2023-02-18 19:36:03 -0400

    Realizo las relaciones entre los models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    //Relacion de muchos a uno
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //Relacio polimorfica
+
+    public function image(){
+        return $this->morphOne(Image::class,'imageable');
+    }
 }
